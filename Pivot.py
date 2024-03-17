b674200035fa86b1646d23919f573e7cd7d4d105
Dorin Gora <157317644+pydorin@users.noreply.github.com>
2024-03-17 19:58:48 +0200

    Update Pivot.py

--- Pivot.py
+++ Pivot.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 # Read the excel file into data frame
-excell_file_location = '/Users/dd/Desktop/3Y.xlsx'
+excell_file_location = 'https://github.com/pydorin/Pd_pivot_table/commit/82be387759bde3aa0d79563c797e7ff839567dc8'
 df = pd.read_excel(excell_file_location)
 
 # # Check for missing values
