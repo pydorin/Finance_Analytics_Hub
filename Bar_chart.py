69e8bed98da830763e0c8fa4755eb6439c80786f
Dorin Gora <157317644+pydorin@users.noreply.github.com>
2024-03-18 23:00:24 +0200

    Add files via upload

---
+++ Bar_chart.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Read the excell file
+data = pd.read_excel('/Users/dd/Desktop/3Y.xlsx')
+
+# Group data by Year and Region
+grouped_data = data.groupby(['Month', 'Region'])['Feet amount'].sum().unstack()
+
+# Create bar chart
+ax = grouped_data.plot(kind='bar', figsize=(10, 6))
+
+# Add labels and title
+plt.xlabel('Month')
+plt.ylabel('Total Feet Amount')
+plt.title('Total Feet Amount by Month and Region')
+
+# Add legend
+plt.legend(title='Region')
+
+# Rotate x-axis labels
+plt.xticks(rotation=60)
+
+# Add labels on top of the bars
+for p in ax.patches:
+    ax.annotate(str(int(p.get_height())), (p.get_x() * 1.005, p.get_height() * 1.005))
+
+plt.tight_layout()
+plt.show()
